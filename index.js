82b4c4be5b3b4f1d5513ab64c488da80fa9c2b16
xsthunder <xsthunder@outlook.com>
2019-12-10 16:19:14 +0800

    disable send file, and cache

--- index.js
+++ index.js
@@ -35,48 +35,71 @@ function getClientIp(req) {
 app.use((req,res,next)=>{
 	let s = (getClientIp(req));
 	let ip = s.substr(s.lastIndexOf(':')+1);
-	console.log(s, ip);
     // next()
 	if(s == '::1' || ip == "127.0.0.1" || ip == "192.168.1.5" || ip == "192.168.1.6" || ip == "192.168.1.7"|| ip == "192.168.1.103"){
 		next();
 	}
 	else res.send("haha");
 });
-app.get('/pack',(req,res)=>{
-	let output = fs.createWriteStream(path);
-	output.on('close',()=>{
-		res.setHeader('Content-Disposition',contentDisposition(filename));
-		res.sendFile(path, getfile());
-	});
-	output.on('error',(err)=>{
-		res.status(500).send(err);
-	});
 
-	let arch = archiver('zip',{zilb:{level:9}});
-	arch.pipe(output);
-	arch.directory('files/',false);
-	arch.finalize();
-});
-app.get('/list',(req,res)=>{
+// no longer in use
+// app.get('/pack',(req,res)=>{
+// 	let output = fs.createWriteStream(path);
+// 	output.on('close',()=>{
+// 		res.setHeader('Content-Disposition',contentDisposition(filename));
+// 		res.sendFile(path, getfile());
+// 	});
+// 	output.on('error',(err)=>{
+// 		res.status(500).send(err);
+// 	});
+
+// 	let arch = archiver('zip',{zilb:{level:9}});
+// 	arch.pipe(output);
+// 	arch.directory('files/',false);
+// 	arch.finalize();
+// });
+
+// see https://glitch.com/edit/#!/server-side-cache-express?path=server.js:9:0
+var mcache = require('memory-cache');
+var cache = (duration) => {
+  return (req, res, next) => {
+	let key = '__express__' + (req.originalUrl || req.url) + (req.body.filename?req.body.filename:'')
+    let cachedBody = mcache.get(key)
+    if (cachedBody) {
+      res.send(cachedBody)
+      return
+    } else {
+      res.sendResponse = res.send
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
+
+app.get('/list', cache(5 * 60)/*5 minute*/, (req,res)=>{
 	fs.readdir(filepath,(err,files)=>{
 		if(err){res.status(400).send(err);return;}
 		res.send(files);
 	});
 });
-app.post('/getfile',(req,res)=>{
+
+// avoid accident load of large size file
+app.post('/getfile', cache(5 * 60)/*5 minute*/, (req, res) => {
 	//Content-Type: application/x-www-form-urlencoded
 	res.setHeader('Content-Disposition',contentDisposition(filename));
-	log(req.body);
 	if(!req.body.filename){
 		res.status(400).send('required field filename');
 		return;
 	}
-	console.log(filepath + req.body.filename +'/');
 	fs.readdir( filepath + req.body.filename +'/', (err,files)=>{
 		if(err){
-			res.sendFile(filepath+req.body.filename, {
-				filename:req.body.filename
-			});
+			// res.sendFile(filepath+req.body.filename, {
+			// 	filename:req.body.filename
+			// });
+			res.send('reading file content is disabled')
 		}
 		else {
 			res.send(files);
